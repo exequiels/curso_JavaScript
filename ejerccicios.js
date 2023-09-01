6fe14617fe359463d366460d0ee56798e4cf1189
exe <exequiel.sabatie@enarvi.com>
2023-09-01 17:03:14 -0300

    Ejercicio 1
    /* Peliculas favoritas */

---
+++ ejerccicios.js
@@ -0,0 +1,10 @@
+/* Peliculas favoritas */
+var usuario = {
+    nombre: "Exequiel",
+    peliculasFavoritas: [
+        {Titulo: "Nueve reinas", Año: "1995"}, 
+        {Titulo: "El hijo de la novia", Año: "2001"},  
+        {Titulo: "The Matrix", Año: "2000"},  
+    ]
+} 
+console.log(usuario.peliculasFavoritas[1]);
