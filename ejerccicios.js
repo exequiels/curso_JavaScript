aca1af137bc514d49a54f5a3ebf190d573b42335
exe <exequiel.sabatie@enarvi.com>
2023-09-01 17:31:59 -0300

    Ejercicio 2
    Contactos

--- ejerccicios.js
+++ ejerccicios.js
@@ -8,3 +8,28 @@ var usuario = {
     ]
 } 
 console.log(usuario.peliculasFavoritas[1]);
+
+/* Gestor de contactos */
+var contactos = {
+    contacto_1 : 
+        {
+         Nombre: "Eluno",
+         Telefono: "11111", 
+         Email: "1111@email.com"
+        }, 
+    contacto_2 :
+        {
+        Nombre: "Eldos", 
+        Telefono: "22222", 
+        Email: "2222@email.com"
+        }, 
+    }
+
+var contacto_3 = {
+    Nombre: "Eltres",
+    Telefono: "33333",
+    Email: "3333@email.com"
+};
+
+contactos.contacto_3 = contacto_3;
+console.log(Object.keys(contactos).length);
